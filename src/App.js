b16a9afa90725347d76988c842733d5d4a84fdf0
cleestofuh <filteik@gmail.com>
2020-12-27 00:08:11 -0800

    header section and dark-light mode done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import './App.css';
-import me from './images/prof.png';
+import Header from './Header.js';
+import Card from './Card.js';
+
+
+const projectList = [
+  {
+    id: '1',
+    title: 'Linkedin',
+    subtitle: 'Senior UX Engineer',
+    desc: 'description'
+  }
+]
 
 function App() {
   return (
     <div className="wrapper">
-      <div class="header-container">
-        <div className="blurb-container">
-            <div className="title">
-              <h1 className="name">Christopher Lo ☕️</h1>
-              <p>designer ⌁ engineer</p>
-              <p>I design and build enterprise products at <a href="https://linkedin.com">Linkedin</a>.</p>
-              <p>Previously designing B2B2C solutions at <a href="https://whova.com">Whova</a> and developing web experiences at <a href="https://www.hjf.org">HJF</a>. </p>
-            </div>
-        </div>
-        <div className="photo-container">
-          <img src={me} alt="me" className="me"/>
-        </div>
-      </div>
+      <Header />
+      <Card />
     </div>
   );
 }
---
+++ src/Card.js
@@ -0,0 +1,17 @@
+
+
+
+
+export default function Card() {
+    return (
+    <div className="card-container">
+        <div className="blurb-container">
+            <div className="title">
+            <h2 className="name">TItle</h2>
+            <p>description</p>
+            </div>
+        </div>
+    </div>
+
+    );
+}
