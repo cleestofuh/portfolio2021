dc43c377d015e9aad7cd5d701c44f249aebbbbea
cleestofuh <filteik@gmail.com>
2021-03-23 09:45:54 -0700

    fixed link errors

--- src/App.js
+++ src/App.js
@@ -23,10 +23,10 @@ function App() {
             <div className="fade-in">
               <p>I am a designer, an engineer, and a problem solver, but most importantly, I'd say I'm an overall pretty cool human being just like you. 😉</p>
               <p>Some things that excite me are design systems, accessibility, and creating moments of <span className="delight">delight</span>.</p>
-              <p>I'm currently working on building enterprise solutions at <a href="https://linkedin.com/in/cleestofuh" target="_blank" rel="noopener">Linkedin</a> that help employees be more productive at scale.</p>
+              <p>I'm currently working on building enterprise solutions at <a href="https://linkedin.com/in/cleestofuh" target="_blank" rel="noreferrer">Linkedin</a> that help employees be more productive at scale.</p>
               <p>At the same time I'm working on getting my Masters in Human Computer Interaction and Design at UC Irvine and plan to graduate in September 2021.</p>
-              <p>Previously, I worked as a Product Designer at <a href="https://whova.com" target="_blank" rel="noopener">Whova</a> designing B2B2C products in the event tech industry and also spent some time working as a 
-                  Web Developer for <a href="https://www.hjf.org/" target="_blank" rel="noopener">Henry M. Jackson Foundation</a>.</p>
+              <p>Previously, I worked as a Product Designer at <a href="https://whova.com" target="_blank" rel="noreferrer">Whova</a> designing B2B2C products in the event tech industry and also spent some time working as a 
+                  Web Developer for <a href="https://www.hjf.org/" target="_blank" rel="noreferrer">Henry M. Jackson Foundation</a>.</p>
               <p>When I'm not making jokes to my friends, I enjoy going outside, staying inside, and sometimes even standing at the doorway so I can do both at the same time.</p>
             </div>
           </div> 
@@ -34,7 +34,7 @@ function App() {
             <div className="fade-in">
               <p>Let's talk about boba, coffee, design, fashion, fitness, frog memes, pc gaming, travel, or literally anything else!</p>
               <p>🍵 ∽ ☕ ∽ 🎨 ∽ 👘 ∽ 🏸 ∽ 🐸 ∽ 🎮 ∽ 🌎</p>
-              <p>Slide into my DMs on <a href="https://linkedin.com/in/cleestofuh" target="_blank" rel="noopener">Linkedin</a> or just shoot me an <a href="chrisedlo@gmail.com" target="_blank" rel="noopener">email</a>.</p>
+              <p>Slide into my DMs on <a href="https://linkedin.com/in/cleestofuh" target="_blank" rel="noreferrer">Linkedin</a> or just shoot me an <a href="chrisedlo@gmail.com" target="_blank" rel="noopener">email</a>.</p>
             </div>
           </div> 
         </Tabs> 
